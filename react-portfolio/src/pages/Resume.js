9b839adb3dae71ace8b51d38ead53bef9c254597
Valeriya <valeriya.finogeeva@gmail.com>
2023-04-19 01:23:45 +1000

    Add README file

--- react-portfolio/src/pages/Resume.js
+++ react-portfolio/src/pages/Resume.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
+import pdfFile from '../assets/resume.pdf';
 
 function Resume() {
-  return (
-    <div>
-      <p>Download My Resume</p>
-      <div className="skills">
-
-      </div>
-    </div>
-  )
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = pdfFile;
+        link.download = './asssets/resume.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+    return (
+        <div>
+            <p>Download My Resume</p>
+            <button onClick={handleDownload}>
+                Download PDF
+            </button>
+        </div>
+    )
 }
 
 export default Resume
